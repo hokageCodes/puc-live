75ffb5daed5da2718b1705a3728e098d9e523172
hokageCodes <hashsenju94@gmail.com>
2026-04-20 07:28:56 +0100

    g

--- app/api/uploadthing/route.js
+++ app/api/uploadthing/route.js
@@ -1,1 +1,11 @@
 // Uploadthing API route removed. Use Cloudinary or other provider instead.
+
+export async function GET() {
+	return new Response('Not implemented', { status: 501 });
+}
+
+export async function POST() {
+	return new Response('Not implemented', { status: 501 });
+}
+
+export {};
